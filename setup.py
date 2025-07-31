5836a0d355deda68be5140ef1f78f568ed587e15
Phuc <haizzhuoc09876@gmail.com>
2025-07-31 15:17:54 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+    setup(
+        name = "demo_1",        
+        version = "",
+        package = find_packages("src"),
+        package_dir={"":"src"},
+    )
+    
