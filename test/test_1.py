62af05b3c847fd0fd1a12f3908e782d25bece5bd
Phuc <phucngo2172016@gmail.com>
2025-08-01 17:10:37 +0000

    test

--- test/test_1.py
+++ test/test_1.py
@@ -1,12 +1,11 @@
 from src import handler
+import json
 
 def test_create_note():
-    event = {"httpMethod": "POST", "body": '{\"text\": \"Test note\"}'}
+    event = {"httpMethod": "POST", "path": "/notes", 
+            "body": json.dumps({"text": "Test note", "id": "note1"})}
     res = handler.main(event, None)
+    print("DEBUG RESPONSE:", json.dumps(res, indent=2))
     assert res['statusCode'] == 200
-
-
-
-
 
 
