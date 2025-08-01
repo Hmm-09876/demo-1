e0a333ddc7a6e5df151fb99f750fb7ca964123d3
Phuc <phucngo2172016@gmail.com>
2025-08-01 12:31:55 +0000

    test

--- localstack/localstack.py
+++ localstack/localstack.py
@@ -1,17 +1,54 @@
-import subprocess, time
+# import subprocess, time
 
 
-subprocess.run(["docker-compose", "up", "-d"], cwd="/home/test/demo-1/localstack")
+# subprocess.run(["docker-compose", "up", "-d"], cwd="/home/test/demo-1/localstack")
 
 
+# time.sleep(30)
+
+
+# subprocess.run([
+#     "awslocal", "--endpoint-url=http://localhost:4566", "s3api", "create-bucket", 
+#     "--bucket", "notes-bucket"
+# ])
+
+
+# subprocess.run([
+#     "awslocal", "--endpoint-url=http://localhost:4566", "lambda", "create-function",
+#     "--function-name", "notesHandler", 
+#     "--runtime", "python3.10",
+#     "--handler", "handler.main",
+#     "--zip-file", "fileb://function.zip",
+#     "--role", "arn:aws:iam::123456789012:role/lambda"
+# ])
+
+
+# print("Deployed")
+
+
+
+
+
+import subprocess
+import time
+import os
+
+# nếu bạn vẫn muốn dùng docker-compose, trường hợp local:
+compose_dir = os.path.dirname(__file__)  # sẽ là demo-1/localstack
+subprocess.run(
+    ["docker-compose", "up", "-d"],
+    cwd=compose_dir,
+    check=True
+)
+
+# chờ LocalStack khởi động
 time.sleep(30)
 
-
+# rồi deploy qua awslocal
 subprocess.run([
     "awslocal", "--endpoint-url=http://localhost:4566", "s3api", "create-bucket", 
     "--bucket", "notes-bucket"
-])
-
+], check=True)
 
 subprocess.run([
     "awslocal", "--endpoint-url=http://localhost:4566", "lambda", "create-function",
@@ -20,7 +57,4 @@ subprocess.run([
     "--handler", "handler.main",
     "--zip-file", "fileb://function.zip",
     "--role", "arn:aws:iam::123456789012:role/lambda"
-])
-
-
-print("Deployed")
+], check=True)
