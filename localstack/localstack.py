875edcb6ce0d480ac4892a9c97e6b750db99d114
Phuc <haizzhuoc09876@gmail.com>
2025-08-18 17:32:09 +0000

    Tested with act push

--- localstack/localstack.py
+++ localstack/localstack.py
@@ -27,32 +27,27 @@
 
 
 
-
+#########################################################################################
 
 # import subprocess
 # import time
 # import os
 
-# # Lấy chính xác thư mục đang chứa file này:
 # compose_dir = os.path.dirname(os.path.abspath(__file__))
 
-# # 1) Nếu bạn vẫn muốn dùng docker-compose:
 # subprocess.run(
 #     ["docker-compose", "up", "-d"],
 #     cwd=compose_dir,
 #     check=True
 # )
 
-# # Chờ LocalStack khởi động (bạn có thể điều chỉnh thời gian hoặc dùng health-check)
 # time.sleep(30)
 
-# # 2) Deploy S3 bucket
 # subprocess.run([
 #     "awslocal", "--endpoint-url=http://localhost:4566", "s3api", "create-bucket",
 #     "--bucket", "notes-bucket"
 # ], check=True)
 
-# # 3) Deploy Lambda
 # subprocess.run([
 #     "awslocal", "--endpoint-url=http://localhost:4566", "lambda", "create-function",
 #     "--function-name", "notesHandler",
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-    setup(
-        name = "demo_1",        
-        version = "",
-        package = find_packages("src"),
-        package_dir={"":"src"},
-    )
+setup(
+    name = "demo_1",        
+    version = "",
+    package = find_packages("src"),
+    package_dir={"":"src"},
+)
     
